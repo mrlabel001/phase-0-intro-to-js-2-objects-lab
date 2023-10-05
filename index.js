c68cc6b8099774102e7781a11e8fc1ffed0d8a76
mrlabel001 <farbean.makini@student.moringaschool.com>
2023-10-05 11:04:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name:"jaspher",
+    streetAddress:"langas"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, { [key]: value });
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newOb =  Object.assign({}, employee);
+    delete newOb[key];
+    return newOb;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+    }
+ 
